0aaccc8b59f778c9afb13a1c55db58629002bbd0
Rafael Zimmermann <rafael.zimmermann@trivago.com>
2021-12-09 21:57:44 +0100

    Add total to the middle of the donut chart

--- walletwatcher-nginx/static/js/donut.js
+++ walletwatcher-nginx/static/js/donut.js
@@ -18,45 +18,73 @@ var config = {
   options: {
     responsive: true,
     aspectRatio: 1,
+    borderColor: '#292929',
+    offset: 2,
+    cutout: "97%",
     plugins: {
       legend: {
-        position: 'top',
-        labels: {
-          color: 'White'
-        }
+        display: false
       },
-      title: {
+      centerTitle: {
         display: true,
-        text: 'Balance',
+        text: '',
         color: 'White'
       }
     }
   },
 };
 
+function drawTotals(chart, options) {
+  let width = chart.chartArea.width;
+  let height = chart.chartArea.height;
+  let ctx = chart.ctx;
+
+  ctx.restore();
+  let fontSize = (height / 130).toFixed(2);
+  ctx.font = fontSize + "em Roboto,sans-serif";
+  ctx.fillStyle = options.color;
+  ctx.textBaseline = "middle";
+
+  let textX = Math.round((width - ctx.measureText(options.text).width) * 0.5);
+  let textY = height / 2;
+  ctx.fillText(options.text, textX, textY);
+  ctx.save();
+}
+
+var centerTitlePlugin = {
+  id: 'centerTitle',
+  beforeDraw: function(chart, args, options) {
+    if (options.display === true) {
+      drawTotals(chart, options);
+    }
+  }
+}
+
 var getSimpleBalance = function() {
-    return fetch("/v1/balance")
-            .then(response => response.json())
-            .then(function(balance) {
-                var result = {};
-                for (var wallet in balance) {
-                    for (var ticker in balance[wallet]) {
-                        if (result.hasOwnProperty(ticker)) {
-                            result[ticker] += balance[wallet][ticker]["EUR"];
-                        } else {
-                            result[ticker] = balance[wallet][ticker]["EUR"];
-                        }
-                    }
+  return fetch("/v1/balance")
+    .then(response => response.json())
+    .then(function(balance) {
+        var result = {};
+        for (var wallet in balance) {
+            for (var ticker in balance[wallet]) {
+                if (result.hasOwnProperty(ticker)) {
+                    result[ticker] += balance[wallet][ticker]["EUR"];
+                } else {
+                    result[ticker] = balance[wallet][ticker]["EUR"];
                 }
-                return result;
-            });
+            }
+        }
+        return result;
+    });
 }
 
 var updateChart = function(balance) {
     tickers = Object.keys(balance);
     balances = [];
+    total = 0;
     tickers.forEach((t) => {
         balances.push(balance[t]);
+        total += balance[t];
     });
     const data = {
       labels: tickers,
@@ -69,6 +97,8 @@ var updateChart = function(balance) {
       ]
     };
     config.data = data;
+    config.options.plugins.centerTitle.text = "€" + total.toFixed(2);
+    Chart.register(centerTitlePlugin)
     const ctx = document.getElementById('balance').getContext('2d');
     const myChart = new Chart(ctx, config);
 }
